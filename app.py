4a56eb69b8abd143c1c0764ebe2df598b5fa9881
@m-tappx <malvarez@techsoulogy.com>
2023-07-04 09:50:34 +0200

    removed streamlit

--- app.py
+++ app.py
@@ -17,7 +17,8 @@ kmeans = KMeans(n_clusters=23, n_init='auto', random_state=42).fit_predict(X_tsn
 
 fig = px.scatter_3d(x=X_tsne[:,0], y=X_tsne[:,1], z=X_tsne[:,2], color=kmeans, size_max=0.1, color_continuous_scale='turbo', labels=kmeans)
 fig.update_layout(width=700, height=700)
+fig.show()
 
-st.title('Text clustering')
-st.write('Text clustering is to automatically group textual documents (for example, documents in plain text, web pages, emails and etc) into clusters based on their content similarity.')
-st.plotly_chart(fig, theme=None, use_container_width=False)
+# st.title('Text clustering')
+# st.write('Text clustering is to automatically group textual documents (for example, documents in plain text, web pages, emails and etc) into clusters based on their content similarity.')
+# st.plotly_chart(fig, theme=None, use_container_width=False)
